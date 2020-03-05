bb76b5c3e543228c61c13252ef40146fac317002
Patrick Weingärtner <patrick.weingaertner91@gmail.com>
2020-03-05 14:17:21 +0100

    listen to r and t to reset the tag or start tracking again by branch

--- main.py
+++ main.py
@@ -5,14 +5,32 @@ import argparse
 from time import sleep
 from signal import signal, SIGINT
 from sys import exit
+from pynput import keyboard
+
+def on_press(key):
+    try:
+        if key.char == 'r':
+            print()
+            subprocess.run(['watson start {project}'.format(project=project)], shell=True)
+        elif key.char == 't':
+            print()
+            initial_branch = subprocess.check_output([git_command], shell=True, text=True).strip()
+            subprocess.run(['watson start {project} +{branch}'.format(project=project, branch=initial_branch)], shell=True)
+    except AttributeError:
+        pass
 
 def handler(signal_received, frame):
+    print()
     print('SIGINT or CTRL-C detected. Exiting gracefully')
     subprocess.run(['watson stop'], shell=True)
     exit(0)
 
+#listen for ctrl+c to stop tiime tracking
 signal(SIGINT, handler)
 
+#listen to key to reset timetracking to no tag
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('project', help='Project name for watson tracking')
